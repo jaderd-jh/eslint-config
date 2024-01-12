a066f871ece248363c0c0748902dd7b6e21ae43b
绝缘子 <jiangang.zhang@jaderd.com>
2024-01-12 14:53:39 +0800

    fix(import): Unable to resolve path to module XXX

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -6,6 +6,7 @@ module.exports = {
   settings: {
     'import/resolver': {
       node: { extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.d.ts'] },
+      typescript: {},
     },
   },
   overrides: basic.overrides,
