b89f676b8e60fc7a7a9980edaa5e90b794a3b13b
pigmon <jiangang.zhang@jaderd.com>
2024-05-11 14:51:32 +0800

    fix: config rule `vue/script-indent`

--- src/configs/vue.ts
+++ src/configs/vue.ts
@@ -179,7 +179,7 @@ export async function vue(
               'vue/operator-linebreak': ['error', 'before'],
               'vue/padding-line-between-blocks': ['error', 'always'],
               'vue/quote-props': ['error', 'consistent-as-needed'],
-              'vue/script-indent': ['error', 2, { baseIndent: 1 }],
+              'vue/script-indent': ['error', 2, { baseIndent: 1, switchCase: 1 }],
               'vue/space-in-parens': ['error', 'never'],
               'vue/template-curly-spacing': 'error',
             }
