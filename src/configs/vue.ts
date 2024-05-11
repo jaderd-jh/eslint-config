ba219f7279aebc52f927537f696f0c8c0c1827ca
pigmon <jiangang.zhang@jaderd.com>
2024-05-11 15:47:44 +0800

    fix: config rule `vue/brace-style`

--- src/configs/vue.ts
+++ src/configs/vue.ts
@@ -164,7 +164,7 @@ export async function vue(
                 multiline: 'always',
                 singleline: 'always',
               }],
-              'vue/brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+              'vue/brace-style': ['error', '1tbs'],
               'vue/comma-dangle': ['error', 'always-multiline'],
               'vue/comma-spacing': ['error', { after: true, before: false }],
               'vue/comma-style': ['error', 'last'],
