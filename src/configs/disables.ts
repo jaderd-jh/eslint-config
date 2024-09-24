d15fe3f5d61dc68c0bf6aaf47ad4e86fc76a4c3d
pigmon <jiangang.zhang@jaderd.com>
2024-09-24 11:33:58 +0800

    fix: glob for disables

--- src/configs/disables.ts
+++ src/configs/disables.ts
@@ -4,7 +4,7 @@ import { GLOB_SRC, GLOB_SRC_EXT } from '../globs'
 export async function disables(): Promise<TypedFlatConfigItem[]> {
   return [
     {
-      files: [`scripts/${GLOB_SRC}`],
+      files: [`**/scripts/${GLOB_SRC}`],
       name: 'jhqn/disables/scripts',
       rules: {
         'no-console': 'off',
@@ -12,7 +12,7 @@ export async function disables(): Promise<TypedFlatConfigItem[]> {
       },
     },
     {
-      files: [`cli/${GLOB_SRC}`, `cli.${GLOB_SRC_EXT}`],
+      files: [`**/cli/${GLOB_SRC}`, `**/cli.${GLOB_SRC_EXT}`],
       name: 'jhqn/disables/cli',
       rules: {
         'no-console': 'off',
