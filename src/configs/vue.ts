b97f181bf0cb02e97900e8f60e3f4ef3bfcbd834
pigmon <jiangang.zhang@jaderd.com>
2024-09-24 11:35:43 +0800

    fix: disable `ts/explicit-function-return-type` in vue files

--- src/configs/vue.ts
+++ src/configs/vue.ts
@@ -103,6 +103,7 @@ export async function vue(
             },
 
         'node/prefer-global/process': 'off',
+        'ts/explicit-function-return-type': 'off',
         'vue/attributes-order': ['error', { alphabetical: true }],
         'vue/block-order': ['error', {
           order: ['script', 'template', 'style'],
