425c086f07a64e3eea0eaf6f04e30fb0c6757620
pigmon <jiangang.zhang@jaderd.com>
2026-02-24 10:43:23 +0800

    feat(react): sync recommend rules

--- src/configs/react.ts
+++ src/configs/react.ts
@@ -58,6 +58,7 @@ export async function react(
 
   const typeAwareRules: TypedFlatConfigItem['rules'] = {
     'react/no-leaked-conditional-rendering': 'warn',
+    'react/no-implicit-key': 'error',
   }
 
   const [
@@ -75,7 +76,7 @@ export async function react(
   const isUsingReactRouter = ReactRouterPackages.some(i => isPackageExists(i))
   const isUsingNext = NextJsPackages.some(i => isPackageExists(i))
 
-  const plugins = pluginReact.configs.all.plugins
+  const plugins = (pluginReact.configs.all as any).plugins
 
   return [
     {
@@ -87,6 +88,7 @@ export async function react(
         'react-hooks-extra': plugins['@eslint-react/hooks-extra'],
         'react-naming-convention': plugins['@eslint-react/naming-convention'],
         'react-refresh': pluginReactRefresh,
+        'react-rsc': plugins['@eslint-react/rsc'],
         'react-web-api': plugins['@eslint-react/web-api'],
       },
     },
@@ -123,9 +125,7 @@ export async function react(
         'react/no-create-ref': 'error',
         'react/no-default-props': 'error',
         'react/no-direct-mutation-state': 'error',
-        'react/no-duplicate-key': 'error',
         'react/no-forward-ref': 'warn',
-        'react/no-implicit-key': 'warn',
         'react/no-missing-key': 'error',
         'react/no-nested-component-definitions': 'error',
         'react/no-nested-lazy-component-declarations': 'error',
@@ -139,10 +139,15 @@ export async function react(
         'react/no-unsafe-component-will-mount': 'warn',
         'react/no-unsafe-component-will-receive-props': 'warn',
         'react/no-unsafe-component-will-update': 'warn',
+        'react/no-unused-class-component-members': 'warn',
         'react/no-use-context': 'warn',
         'react/no-useless-forward-ref': 'warn',
         'react/prefer-use-state-lazy-initialization': 'warn',
+
         'react/prefer-namespace-import': 'error',
+
+        // recommended rules from eslint-plugin-react-rsc https://eslint-react.xyz/docs/rules/overview#rsc-rules
+        'react-rsc/function-definition': 'error',
 
         // recommended rules from eslint-plugin-react-dom https://eslint-react.xyz/docs/rules/overview#dom-rules
         'react-dom/no-dangerously-set-innerhtml': 'warn',
@@ -157,6 +162,20 @@ export async function react(
         'react-dom/no-unsafe-iframe-sandbox': 'warn',
         'react-dom/no-use-form-state': 'error',
         'react-dom/no-void-elements-with-children': 'error',
+
+        // recommended rules from eslint-plugin-react-hooks-extra https://eslint-react.xyz/docs/rules/overview#hooks-extra-rules
+        'react-hooks-extra/no-direct-set-state-in-use-effect': 'warn',
+
+        // recommended rules from eslint-plugin-react-naming-convention https://eslint-react.xyz/docs/rules/overview#naming-convention-rules
+        'react-naming-convention/context-name': 'warn',
+        'react-naming-convention/ref-name': 'warn',
+        'react-naming-convention/use-state': 'warn',
+
+        // recommended rules from eslint-plugin-react-web-api https://eslint-react.xyz/docs/rules/overview#web-api-rules
+        'react-web-api/no-leaked-event-listener': 'warn',
+        'react-web-api/no-leaked-interval': 'warn',
+        'react-web-api/no-leaked-resize-observer': 'warn',
+        'react-web-api/no-leaked-timeout': 'warn',
 
         // recommended rules eslint-plugin-react-hooks https://github.com/facebook/react/blob/main/packages/eslint-plugin-react-hooks/README.md
         // Core hooks rules
@@ -183,15 +202,6 @@ export async function react(
               'react-hooks/incompatible-library': 'warn',
             }
           : {}),
-
-        // recommended rules from eslint-plugin-react-hooks-extra https://eslint-react.xyz/docs/rules/overview#hooks-extra-rules
-        'react-hooks-extra/no-direct-set-state-in-use-effect': 'warn',
-
-        // recommended rules from eslint-plugin-react-web-api https://eslint-react.xyz/docs/rules/overview#web-api-rules
-        'react-web-api/no-leaked-event-listener': 'warn',
-        'react-web-api/no-leaked-interval': 'warn',
-        'react-web-api/no-leaked-resize-observer': 'warn',
-        'react-web-api/no-leaked-timeout': 'warn',
 
         // preconfigured rules from eslint-plugin-react-refresh https://github.com/ArnaudBarre/eslint-plugin-react-refresh/tree/main/src
         'react-refresh/only-export-components': [
@@ -244,7 +254,19 @@ export async function react(
         ...overrides,
       },
     },
-
+    {
+      files: filesTypeAware,
+      name: 'jhqn/react/typescript',
+      rules: {
+        // Disables rules that are already handled by TypeScript
+        'react-dom/no-string-style-prop': 'off',
+        'react-dom/no-unknown-property': 'off',
+        'react/jsx-no-duplicate-props': 'off',
+        'react/jsx-no-undef': 'off',
+        'react/jsx-uses-react': 'off',
+        'react/jsx-uses-vars': 'off',
+      },
+    },
     ...isTypeAware
       ? [{
           files: filesTypeAware,
